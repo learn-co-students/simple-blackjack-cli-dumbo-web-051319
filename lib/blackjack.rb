63b3a3dadc0aaca069be3ae993776ebd165b49f1
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-04-30 03:15:58 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,11 +23,9 @@ def end_game(card_total)
 end
 
 def initial_round
-  deal_card
-  deal_card
-  return deal_card + deal_card
-  
-  
+  first_round = deal_card + deal_card
+  display_card_total(first_round)
+  return first_round
 end
 
 def hit?
