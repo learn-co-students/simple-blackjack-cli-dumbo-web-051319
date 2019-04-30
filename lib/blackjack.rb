37f7fc7775a475bb46c0eb5f2f66d325ef9ed3b4
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-04-30 03:48:51 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,23 +33,23 @@ def initial_round
 end
 
 def hit?
- prompt_user
- get_user_input
-  until input == h || input == s
+  prompt_user
+  input = get_user_input
+  until input == 'h' || input == 's'
     invalid_command
     prompt_user
     input = get_user_input
   end
-  
-  if input == h
+  if input == 'h'
     card_total += deal_card
-  elsif input == s
+  elsif input == 's'
     card_total
   end
 end
 
+
 def invalid_command
- puts "Please enter a valid command"
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -58,10 +58,10 @@ end
 
 def runner
   welcome
-  initial_round
-  hit?
-  display_card_total
-  
-  end_game
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
 end
-    
